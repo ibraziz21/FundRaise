4bf5b58c3f3f13d88855555254e986aefbe8e71e
Ibrahim <ibraziz21@gmail.com>
2020-03-23 17:38:04 +0300

    Images Working
    Signed-off-by: Ibrahim <ibraziz21@gmail.com>

--- app/src/main/java/com/redbravo/fundraise/campaign.java
+++ app/src/main/java/com/redbravo/fundraise/campaign.java
@@ -1,27 +1,36 @@
 package com.redbravo.fundraise;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.parse.FindCallback;
+import com.parse.GetDataCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.util.List;
 
+import timber.log.Timber;
+
 public class campaign extends AppCompatActivity {
+   ImageView image;
     EditText commitamt;
 TextView text1;
-TextView text2;
+
     TextView amount;
     TextView amountc;
     TextView longdesc;
@@ -32,12 +41,13 @@ TextView text2;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campaign);
         text1=findViewById(R.id.title1);
-        text2=findViewById(R.id.shortDescript);
+
         amount=findViewById(R.id.AmountReq);
         amountc = findViewById(R.id.AmountCont);
         longdesc = findViewById(R.id.Longdesc);
         commitamt= findViewById(R.id.commit);
         button=findViewById(R.id.commitamt);
+        image = findViewById(R.id.image);
         final Intent i = getIntent();
         final String title = i.getStringExtra("title");
 
@@ -51,12 +61,22 @@ TextView text2;
           public void done(List<ParseObject> objects, ParseException e) {
               if (e==null  && objects.size()>0){
                   for(ParseObject object: objects) {
-                      String shortD = object.getString("shortDescription");
+                      ParseFile file= (ParseFile) object.get("Image");
+                      file.getDataInBackground(new GetDataCallback() {
+                                                   @Override
+                                                   public void done(byte[] data, ParseException e) {
+                                                       if (e == null && data != null) {
+                                                           Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
 
+
+                                                           image.setImageBitmap(bitmap);
+                                                       }
+                                                   }
+                                               });
                       String amt = String.valueOf(object.getInt("amountRequired"));
                       String amtc = String.valueOf(object.getInt("amountRaised"));
                       String longd = object.getString("longDescription");
-                      text2.setText(shortD);
+
                       amount.setText("Amount Required: "+amt);
                       amountc.setText("Amount Raised: "+amtc);
                       longdesc.setText(longd);
@@ -73,6 +93,22 @@ button.setOnClickListener(new View.OnClickListener() {
     public void onClick(View v) {
         String commited =commitamt.getText().toString();
         final int amtcommitted = Integer.parseInt(commited);
+
+        ParseObject data =new ParseObject("CommitedAmt");
+        data.put("User", ParseUser.getCurrentUser());
+        data.put("campaignName",title);
+        data.put("Amount",amtcommitted);
+        data.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e==null){
+                    Timber.i("saved");
+                }else{
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("campaigndata");
         query.whereEqualTo("campaignName",title);
--- app/src/main/java/com/redbravo/fundraise/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/redbravo/fundraise/ui/dashboard/DashboardFragment.java
@@ -1,23 +1,41 @@
 package com.redbravo.fundraise.ui.dashboard;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.redbravo.fundraise.R;
 
-public class DashboardFragment extends Fragment {
+import java.io.ByteArrayOutputStream;
+
+public class DashboardFragment extends Fragment  implements View.OnClickListener{
+    Bitmap bitmap;
+ImageView upload;
 EditText campaign;
 EditText shortdesc;
 EditText Amount;
@@ -25,47 +43,151 @@ EditText longdesc;
 Button button;
     private DashboardViewModel dashboardViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-            campaign=root.findViewById(R.id.campaign);
-            shortdesc=root.findViewById(R.id.shortdesc);
-            Amount=root.findViewById(R.id.amt);
-            longdesc=root.findViewById(R.id.LongDesc);
-            button=root.findViewById(R.id.addCamp);
 
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String Campaign=campaign.getText().toString();
-                    String shortDesc=shortdesc.getText().toString();
-                   String X=Amount.getText().toString();
-                   int amount=Integer.parseInt(X);
-                   String longDesc=longdesc.getText().toString();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-                    ParseObject data = new ParseObject("campaigndata");
-                    data.put("campaignName", Campaign);
-                    data.put("shortDescription",shortDesc);
-                    data.put("amountRequired",amount);
-                    data.put("longDescription", longDesc);
-                    data.put("amountRaised", 0);
-                    data.saveInBackground(new SaveCallback() {
-                        @Override
-                        public void done(ParseException e) {
-                            if (e==null){
-                                Toast.makeText(getContext(), "Campaign Started Successfully" , Toast.LENGTH_SHORT).show();
 
-                            }else {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
+        upload = view.findViewById(R.id.imageupload);
+        campaign = view.findViewById(R.id.campaign);
+        button = view.findViewById(R.id.addCamp);
+        shortdesc= view.findViewById(R.id.shortdesc);
+        longdesc = view.findViewById(R.id.LongDesc);
+        Amount =view.findViewById(R.id.amt);
 
+        upload.setOnClickListener(DashboardFragment.this);
+        button.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+
+            case R.id.imageupload:
+                //Getting permission to read the external storage from user
+                if (android.os.Build.VERSION.SDK_INT >= 19 &&
+                        ActivityCompat.checkSelfPermission(getContext(),
+                                Manifest.permission.READ_EXTERNAL_STORAGE)
+                                != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]
+                                    {Manifest.permission.READ_EXTERNAL_STORAGE},
+                            1000);
+                }else{
+                    getChosenImage();
                 }
-            });
 
-        return root;
+                break;
+
+            case R.id.addCamp:
+
+                if (bitmap != null){
+
+                 String projectname = campaign.getText().toString();
+                 String shortd = shortdesc.getText().toString();
+                 String amts = Amount.getText().toString();
+                 int amt = Integer.parseInt(amts);
+                 String longd = longdesc.getText().toString();
+
+                        //Convert the image to an array of bytes (easier to compress and upload it)
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+
+                        byte[] bytes = byteArrayOutputStream.toByteArray();
+                        ParseFile parseFile = new ParseFile("pic.png",bytes);
+                        ParseObject parseObject = new ParseObject("campaigndata");
+                        parseObject.put("campaignName", projectname);
+                        parseObject.put("shortDescription",shortd);
+                        parseObject.put("amountRequired",amt);
+                        parseObject.put("longDescription",longd);
+                        parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+                        parseObject.put("amountRaised",0);
+                        parseObject.put("Image",parseFile);
+
+                        final ProgressDialog dialog = new ProgressDialog(getContext());
+                        dialog.setMessage("Posting...");
+                        dialog.show();
+
+                        parseObject.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if (e==null){
+                                    Toast.makeText(getContext(),"Image Has Been Published !", Toast.LENGTH_LONG).show();
+                                }else{
+                                    Toast.makeText(getContext(),"Error , please try again later.", Toast.LENGTH_SHORT).show();
+                                }
+                                dialog.dismiss();
+                            }
+                        });
+
+
+                } else{
+                    Toast.makeText(getContext(), "You Must Choose A Picture", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+
+
+    }
+
+    private void getChosenImage() {
+
+        Toast.makeText(getContext(), "Choose A Picture", Toast.LENGTH_SHORT).show();
+        Uri imageUri= ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,0);
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 2000);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1000){
+            if (grantResults.length > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                getChosenImage();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2000){
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                //Do Something with your chosen image
+                try{
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath = cursor.getString(columnIndex);
+                    cursor.close();
+                    bitmap = BitmapFactory.decodeFile(picturePath);
+                    upload.setImageBitmap(bitmap);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+        } else {
+            Toast.makeText(getActivity(), "Try Again!!", Toast.LENGTH_SHORT)
+                    .show();
+        }
+
     }
 }
